b1fd77e20b0999c237cee3e2b8d2fbd1925d3620
sadikovi <ivan.sadikov@lincolnuni.ac.nz>
2017-04-03 22:07:46 +1200

    add octicons

--- src/main/java/com/github/lightcopy/ContextProvider.java
+++ src/main/java/com/github/lightcopy/ContextProvider.java
@@ -4,15 +4,25 @@ import java.io.InputStream;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 @Path("/")
 public class ContextProvider {
+  // images
   private static final String INDEX_FAVICON_PATH = "static/favicon.ico";
   private static final String INDEX_LOGO_PATH = "static/logo.png";
-  private static final String INDEX_CSS_PATH = "static/index.min.css";
+  // css files
+  private static final String INDEX_CSS_PATH = "static/{css}.css";
+  // font resources
+  private static final String INDEX_TTF_PATH = "static/{ttf}.ttf";
+  private static final String INDEX_WOFF_PATH = "static/{woff}.woff";
+  private static final String INDEX_EOT_PATH = "static/{eot}.eot";
+  private static final String INDEX_SVG_PATH = "static/{svg}.svg";
+  // javascript
   private static final String INDEX_JS_PATH = "static/index.min.js";
+  // html
   private static final String INDEX_HTML_PATH = "static/index.html";
 
   @GET
@@ -34,9 +44,46 @@ public class ContextProvider {
   @GET
   @Path(INDEX_CSS_PATH)
   @Produces("text/css")
-  public Response getIndexCSS() {
-    InputStream css = this.getClass().getClassLoader().getResourceAsStream(INDEX_CSS_PATH);
-    return Response.ok(css).build();
+  public Response getIndexCSS(@PathParam("css") String css) {
+    String filepath = INDEX_CSS_PATH.replace("{css}", css);
+    InputStream stream = this.getClass().getClassLoader().getResourceAsStream(filepath);
+    return Response.ok(stream).build();
+  }
+
+  @GET
+  @Path(INDEX_TTF_PATH)
+  @Produces("application/octet-stream")
+  public Response getIndexTTF(@PathParam("ttf") String ttf) {
+    String filepath = INDEX_TTF_PATH.replace("{ttf}", ttf);
+    InputStream stream = this.getClass().getClassLoader().getResourceAsStream(filepath);
+    return Response.ok(stream).build();
+  }
+
+  @GET
+  @Path(INDEX_WOFF_PATH)
+  @Produces("application/octet-stream")
+  public Response getIndexWOFF(@PathParam("woff") String woff) {
+    String filepath = INDEX_WOFF_PATH.replace("{woff}", woff);
+    InputStream stream = this.getClass().getClassLoader().getResourceAsStream(filepath);
+    return Response.ok(stream).build();
+  }
+
+  @GET
+  @Path(INDEX_EOT_PATH)
+  @Produces("application/octet-stream")
+  public Response getIndexEOT(@PathParam("eot") String eot) {
+    String filepath = INDEX_EOT_PATH.replace("{eot}", eot);
+    InputStream stream = this.getClass().getClassLoader().getResourceAsStream(filepath);
+    return Response.ok(stream).build();
+  }
+
+  @GET
+  @Path(INDEX_SVG_PATH)
+  @Produces("image/svg+xml")
+  public Response getIndexSVG(@PathParam("svg") String svg) {
+    String filepath = INDEX_EOT_PATH.replace("{svg}", svg);
+    InputStream stream = this.getClass().getClassLoader().getResourceAsStream(filepath);
+    return Response.ok(stream).build();
   }
 
   @GET
--- web/coffee/application.coffee
+++ web/coffee/application.coffee
@@ -2,13 +2,41 @@ React = require 'react'
 ReactDOM = require 'react-dom'
 Reactable = require '../coffee/reactable'
 
-createView = (spec) ->
-  React.createFactory(React.createClass(spec))
+class Header extends Reactable
+  render: ->
+    @div className: "columns",
+      @div(className: "three-fifths column",
+        @span className: "text-bold", "Name"),
+      @div(className: "one-fifth column",
+        @span className: "text-bold", "Modified"),
+      @div(className: "one-fifth column",
+        @span className: "text-bold", "Permissions")
+
+class Row extends Reactable
+  render: ->
+    @div className: "columns",
+      @div(className: "three-fifths column", "#{@props.name}"),
+      @div(className: "one-fifth column", "#{@props.modified}"),
+      @div(className: "one-fifth column", "#{@props.permissions}")
+
+class FileSystemTable extends Reactable
+  render: ->
+    @div className: "three-fourths column",
+      Header.new(),
+      @div(className: "divider"),
+      Row.new(name: "folderA", modified: "2017-01-01 00:00:00", permissions: "rwxrwxrwx"),
+      Row.new(name: "folderB", modified: "2017-01-01 00:00:00", permissions: "rwxrwxrwx")
+
+class Info extends Reactable
+  render: ->
+    @div className: "one-fourth column",
+      @div className: "blankslate clean-background",
+        @p {}, "Select item on the left to view details"
 
 class Application extends Reactable
   render: ->
     @div className: "columns",
-      @div className: "three-fourths column", "File system"
-      @div className: "one-fourth column", "Data"
+      FileSystemTable.new(),
+      Info.new()
 
 ReactDOM.render Application.new(), document.getElementById('view')
